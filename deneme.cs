da8ae0619206015227e2ea6fcc513df782862af7
RadresS <serdar-dnc@yandex.com>
2017-11-23 00:43:00 +0300

    sadasas
    asdasda

--- deneme.cs
+++ deneme.cs
@@ -1,2 +1,6 @@
-warning: LF will be replaced by CRLF in deneme.txt.
-The file will have its original line endings in your working directory.
+namespaces deneme
+{
+public internal class deneme
+{
+}
+}
